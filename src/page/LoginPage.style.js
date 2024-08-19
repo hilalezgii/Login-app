9224e0050d65943cda16a029f0a51d8fd272ee53
hilalezgii <hilal_ezgi_2003@hotmail.com>
2024-08-19 23:32:59 +0300

    feat:designed texinput and added button

--- src/page/LoginPage.style.js
+++ src/page/LoginPage.style.js
@@ -15,10 +15,54 @@ export default StyleSheet.create({
         objectFit: 'contain',
         marginTop: 50,
     },
+    userText:{
+        fontWeight:'700',
+        color:'#6C2A09',
+        marginLeft:16,
+        fontSize:16,
+    },
+
     text:{
         fontSize:20,
         fontWeight: 'bold',
-        color:'#6C2A09'
+        color:'#6C2A09',
+        marginTop: 5,
         
     },
+    inputContainer:{
+        marginTop:80,
+    },
+    TextInput:{
+        height: 50,
+        width:350,
+        outline: 'none',
+        marginHorizontal: 12,
+        marginBottom: 12,
+        marginTop: 4,
+        paddingLeft:12,
+        borderWidth: 1,
+        borderColor:'rgba(108, 42, 9, 0.45)',
+        backgroundColor: 'rgba(108, 42, 9, 0.25)',
+        padding: 5,
+        borderRadius:10,
+        color:'#6C2A09'
+    },
+    button:{
+        height: 50,
+        width:350,
+        outline: 'none',
+        margin: 12,
+        padding: 5,
+        borderRadius:30,
+        backgroundColor:'#FFB901',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText:{
+        color:'#6C2A09',
+        fontWeight:'700',
+
+    },
+
+    
 })
--- src/page/LoginScreen.js
+++ src/page/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './LoginPage.style'
-import { Image, Text, TextInput, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 
 const LoginScreen = () => {
     return (
@@ -9,6 +11,24 @@ const LoginScreen = () => {
                 style={styles.logo}
                 source={require('../assets/coffee.jpg')} />
             <Text style={styles.text}>Giriş Yap, Kahve Keyfini Yaşa!</Text>
+            <View style={styles.inputContainer}>
+
+                <Text style={styles.userText}>E-Mail Adresiniz</Text>
+                <TextInput
+                    placeholderTextColor='#6C2A09'
+                    style={styles.TextInput}
+                    placeholder='Email...' />
+
+                <Text style={styles.userText}>Şifreniz</Text>
+                <TextInput
+                    placeholderTextColor='#6C2A09'
+                    style={styles.TextInput}
+                    placeholder='Şifre...' />
+
+            </View>
+            <TouchableOpacity style={styles.button}>
+                <Text style ={styles.buttonText}>Giriş Yap</Text>
+            </TouchableOpacity>
         </View>
     )
 
