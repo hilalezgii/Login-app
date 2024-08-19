25afa82239bc3a53bbb70de7e1eca1ed5236ff1c
hilalezgii <hilal_ezgi_2003@hotmail.com>
2024-08-19 16:55:32 +0300

    first commit

---
+++ src/page/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './LoginPage.style'
+import { Text, TextInput, View } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>selam</Text>
+        </View>
+    )
+
+}
+export default LoginScreen;
